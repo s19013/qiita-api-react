20843184b700b7e24ab79266babb54718645d5f6
知念英弥 <s19013@std.it-college.ac.jp>
2023-06-26 16:09:36 +0900

    記事情報表示コンポネント

---
+++ src/Components/ArticleComponent.js
@@ -0,0 +1,9 @@
+export default function ArticleComponent({article}) {
+    return (
+        <div className="article">
+            <a href={article.url}>
+                <h2>{article.title}</h2>
+            </a>
+        </div>
+    )
+}
