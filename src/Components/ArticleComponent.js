8beeb38c125a43063f472b602e58de1e1ac9464c
知念英弥 <s19013@std.it-college.ac.jp>
2023-06-27 21:05:47 +0900

    別タブで開くようにしてる

--- src/Components/ArticleComponent.js
+++ src/Components/ArticleComponent.js
@@ -3,7 +3,7 @@ import './ArticleComponent.css'
 export default function ArticleComponent({article}) {
     return (
         <div className="article">
-            <a href={article.url}>
+            <a href={article.url} target="_blank" rel="noopener noreferrer">
                 <h2>{article.title}</h2>
             </a>
         </div>
